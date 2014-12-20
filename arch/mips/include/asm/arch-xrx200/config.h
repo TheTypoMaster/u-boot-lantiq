026f29d93f6a9b9f6b96f7de6a58226eae59dafd
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-12-20 19:58:22 +0100

    MIPS: add support for Lantiq XWAY XRX200 SoC family
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ arch/mips/include/asm/arch-xrx200/config.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ARCH_XRX200_CONFIG_H
+#define __ARCH_XRX200_CONFIG_H
+
+/* CPU */
+#define CONFIG_SYS_MIPS_TIMER_FREQ	250000000
+
+/* RAM */
+#define CONFIG_NR_DRAM_BANKS		1
+
+/* SRAM */
+#define CONFIG_SYS_SRAM_BASE		0xbe220000
+#define CONFIG_SYS_SRAM_SIZE		0x10000
+
+/* U-Boot */
+#if defined(CONFIG_LANTIQ_BOOT_RAM)
+#define CONFIG_SYS_TEXT_BASE		0xa0100000
+#define CONFIG_SYS_INIT_SP_ADDR		0xa0104000
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#endif
+
+#endif /* __ARCH_XRX200_CONFIG_H */
