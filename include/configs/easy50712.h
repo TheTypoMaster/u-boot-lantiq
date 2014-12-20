f626ef78c8ddecad7bdb9892c517907ee18a8666
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-12-20 19:58:23 +0100

    MIPS: add Lantiq EASY50712 board
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/easy50712.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <asm/arch-lantiq/config.h>
+#include <asm/arch/config.h>
+
+#define CONFIG_DISPLAY_BOARDINFO
+
+#define CONFIG_SYS_LOAD_ADDR		0x81000000
+
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_SIZE			(8 * 1024)
